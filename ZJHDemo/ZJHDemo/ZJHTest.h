e893742ae4974da957ff29e27e141383bb2a5bbd
沉默学飞翔 <1035044809@qq.com>
2018-11-24 10:22:30 +0800

    submit

---
+++ ZJHDemo/ZJHDemo/ZJHTest.h
@@ -0,0 +1,18 @@
+//
+//  ZJHTest.h
+//  ZJHDemo
+//
+//  Created by 孙云飞 on 2018/11/24.
+//  Copyright © 2018 孙云飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJHTest : NSObject
+
+- (void)test;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZJHDemo/ZJHDemo/ZJHTest.m
@@ -0,0 +1,16 @@
+//
+//  ZJHTest.m
+//  ZJHDemo
+//
+//  Created by 孙云飞 on 2018/11/24.
+//  Copyright © 2018 孙云飞. All rights reserved.
+//
+
+#import "ZJHTest.h"
+
+@implementation ZJHTest
+- (void)test{
+    
+    NSLog(@"test,组件化的开发测试调用");
+}
+@end
